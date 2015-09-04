2e8c7a802816c23620af28c36280e35b619ecc3d
Brian Pollack <brian.pollack@cern.ch>
2015-09-04 16:06:07 -0500

    experimenting with fitting procedures

--- tools/fit_funcs.py
+++ tools/fit_funcs.py
@@ -21,7 +21,9 @@ def bz_r0_1d(z, *params):
 
   return bz
 
-def bz_r0_1d_lm(z, R, A0,B0,A1,B1,A2,B2,A3,B3,A4,B4,A5,B5,A6,B6):
+def bz_r0_1d_lm(z, R, A0,B0,A1,B1,A2,B2,A3,B3,A4,B4,
+    A5,B5,A6,B6,A7,B7,A8,B8,A9,B9,A10,B10,A11,B11,
+    A12,B12,A13,B13,A14,B14):
   """ model decaying sine wave, subtract data"""
   R = R
   b_zeros = special.jn_zeros(0,(bz_r0_1d_lm.func_code.co_argcount-2)/2)
@@ -32,7 +34,15 @@ def bz_r0_1d_lm(z, R, A0,B0,A1,B1,A2,B2,A3,B3,A4,B4,A5,B5,A6,B6):
       kms[3]*(A3*np.exp(kms[3]*z) - B3*np.exp(-kms[3]*z))+
       kms[4]*(A4*np.exp(kms[4]*z) - B4*np.exp(-kms[4]*z))+
       kms[5]*(A5*np.exp(kms[5]*z) - B5*np.exp(-kms[5]*z))+
-      kms[6]*(A6*np.exp(kms[6]*z) - B6*np.exp(-kms[6]*z))
+      kms[6]*(A6*np.exp(kms[6]*z) - B6*np.exp(-kms[6]*z))+
+      kms[7]*(A7*np.exp(kms[7]*z) - B7*np.exp(-kms[7]*z))+
+      kms[8]*(A8*np.exp(kms[8]*z) - B8*np.exp(-kms[8]*z))+
+      kms[9]*(A9*np.exp(kms[9]*z) - B9*np.exp(-kms[9]*z))+
+      kms[10]*(A10*np.exp(kms[10]*z) - B10*np.exp(-kms[10]*z))+
+      kms[11]*(A11*np.exp(kms[11]*z) - B11*np.exp(-kms[11]*z))+
+      kms[12]*(A12*np.exp(kms[12]*z) - B12*np.exp(-kms[12]*z))+
+      kms[13]*(A13*np.exp(kms[13]*z) - B13*np.exp(-kms[13]*z))+
+      kms[14]*(A14*np.exp(kms[14]*z) - B14*np.exp(-kms[14]*z))
       )
   return model
 
