c00258f8ca551786344bbc69aa5012a81ff40ddc
Brian Pollack <brian.pollack@cern.ch>
2016-08-18 17:16:27 -0500

    testing out bootstrap theme

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
+import sphinx_bootstrap_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -129,7 +129,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -138,8 +138,8 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#html_theme_path = ['_themes',]
+html_theme_path = [sphinx_bootstrap_theme.get_html_theme_path()]
+# html_theme_path = ['_themes',]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
@@ -164,7 +164,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_themes']
+# html_static_path = ['_themes']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
