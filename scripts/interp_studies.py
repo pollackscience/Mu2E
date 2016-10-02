132d4750eb82cddb9586adcd4e6d0d6b8f927bc0
Brian Pollack <brianleepollack@gmail.com>
2016-10-02 14:31:46 -0500

    testing in linux

--- scripts/interp_studies.py
+++ scripts/interp_studies.py
@@ -5,12 +5,20 @@ import plotly.graph_objs as go
 from mu2e.offline import init_notebook_mode, iplot
 
 
-def interp_phi(df, x, y, z, plot=True):
+def interp_phi(df, x, y, z, df_alt=None, plot=True):
     """First thing's first. Plot the xyz point, and the immediate cube around it."""
 
     df_trimmed = df.query('{0}<=X<={1} and {2}<=Y<={3} and {4}<=Z<={5}'.format(
         x-25, x+25, y-25, y+25, z-25, z+25))
     df_trimmed = df_trimmed[['X', 'Y', 'Z', 'Bx', 'By', 'Bz']]
+
+    if df_alt is None:
+        df_true = df_trimmed.query('X=={0} and Y=={1} and Z=={2}'.format(
+            x, y, z))
+    else:
+        df_true = df_alt.query('{0}<=X<={1} and {2}<=Y<={3} and {4}<=Z<={5}'.format(
+            x-1e-9, x+1e-9, y-1e-9, y+1e-9, z-1e-9, z+1e-9))
+
     df_trimmed = df_trimmed.query('X!={0} and Y!={1} and Z!={2}'.format(
         x, y, z))
 
@@ -21,7 +29,29 @@ def interp_phi(df, x, y, z, plot=True):
     x_rel = (x - df_trimmed.ix[0].X) / (df_trimmed.ix[4].X-df_trimmed.ix[0].X)
     y_rel = (y - df_trimmed.ix[0].Y) / (df_trimmed.ix[2].Y-df_trimmed.ix[0].Y)
     z_rel = (z - df_trimmed.ix[0].Z) / (df_trimmed.ix[1].Z-df_trimmed.ix[0].Z)
-    print x_rel, y_rel, z_rel
+
+    print x_rel
+    bx_interp = ((1-y_rel)*(1-z_rel)*(x_rel*df_trimmed.ix[7].Bx + (1-x_rel)*df_trimmed.ix[3].Bx) +
+                 y_rel * (1-z_rel) * (x_rel*df_trimmed.ix[5].Bx + (1-x_rel)*df_trimmed.ix[1].Bx) +
+                 (1-y_rel) * z_rel * (x_rel*df_trimmed.ix[6].Bx + (1-x_rel)*df_trimmed.ix[2].Bx) +
+                 y_rel * z_rel * (x_rel*df_trimmed.ix[4].Bx + (1-x_rel)*df_trimmed.ix[0].Bx))
+
+    by_interp = ((1-x_rel)*(1-z_rel)*(y_rel*df_trimmed.ix[7].By + (1-y_rel)*df_trimmed.ix[5].By) +
+                 x_rel * (1-z_rel) * (y_rel*df_trimmed.ix[3].By + (1-y_rel)*df_trimmed.ix[1].By) +
+                 (1-x_rel) * z_rel * (y_rel*df_trimmed.ix[6].By + (1-y_rel)*df_trimmed.ix[4].By) +
+                 x_rel * z_rel * (y_rel*df_trimmed.ix[2].By + (1-y_rel)*df_trimmed.ix[0].By))
+
+    bz_interp = ((1-x_rel)*(1-y_rel)*(z_rel*df_trimmed.ix[7].Bz + (1-z_rel)*df_trimmed.ix[6].Bz) +
+                 x_rel * (1-y_rel) * (z_rel*df_trimmed.ix[3].Bz + (1-z_rel)*df_trimmed.ix[2].Bz) +
+                 (1-x_rel) * y_rel * (z_rel*df_trimmed.ix[5].Bz + (1-z_rel)*df_trimmed.ix[4].Bz) +
+                 x_rel * y_rel * (z_rel*df_trimmed.ix[1].Bz + (1-z_rel)*df_trimmed.ix[0].Bz))
+
+    bx_err = (df_true.Bx.values[0]-bx_interp)/df_true.Bx.values[0]
+    by_err = (df_true.By.values[0]-by_interp)/df_true.By.values[0]
+    bz_err = (df_true.Bz.values[0]-bz_interp)/df_true.Bz.values[0]
+    print 'Bx', df_true.Bx.values[0], bx_interp, bx_err
+    print 'By', df_true.By.values[0], by_interp, by_err
+    print 'Bz', df_true.Bz.values[0], bz_interp, bz_err
 
     if plot:
         init_notebook_mode()
@@ -65,7 +95,7 @@ def interp_phi(df, x, y, z, plot=True):
                 r=0,
                 b=0,
                 t=0
-            )
+            ),
         )
         fig = go.Figure(data=data, layout=layout)
         iplot(fig)
